ebe5fe6e5b4a5e853ff8ab9f48654851313a9d2f
zhouwei <zhouweichild@163.com>
2020-01-15 14:22:13 +0800

    第二次提交

---
+++ work/dict_project/dict_insert.py
@@ -0,0 +1,19 @@
+import pymysql
+import re 
+
+f = open('dict.txt')
+db = pymysql.connect('localhost','root','pcms','dict')
+
+cursor = db.cursor()
+
+for line in f:
+	l = re.split(r'\s+',line)
+	word = l[0]
+	interpret = ' '.join(l[1:])
+	sql = "insert into words(word,interpret) values ('%s','%s')"%(word,interpret)
+	try:
+		cursor.execute(sql)
+		db.commit()
+	except:
+		db.rollback()
+f.close() 	
---
+++ work/dict_project/dict_server.py
@@ -0,0 +1,197 @@
+#coding=utf-8
+'''
+name:zhouwei
+date:2020-1-3
+email:zhouweichild@163.com
+modules:pymysql
+This is a dict project for AID 
+'''
+
+from socket import * 
+import os 
+import time 
+import signal
+import pymysql 
+import sys 
+
+#定义需要的全局变量
+DICT_TEXT = 'dict.txt'
+HOST = '0.0.0.0'
+PORT = 8000
+ADDR = (HOST,PORT)
+
+#流程控制
+def main():
+	#创建数据库连接
+	db = pymysql.connect('localhost','root','pcms','dict')
+	#创建套接字
+	s = socket()
+	s.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
+	s.bind(ADDR)
+	s.listen(5)
+
+	#忽略子进程信号
+	signal.signal(signal.SIGCHLD,signal.SIG_IGN)
+	
+	while True:
+		try:
+			c,addr = s.accept()
+		except KeyboardInterrupt:
+			s.close()
+			sys.exit("服务器退出")
+		except Exception as e:
+			print(e)
+			continue 
+		
+		#创建子进程
+		pid = os.fork()
+		if pid == 0:
+			s.close()
+			do_child(c,db)
+		else:
+			c.close()
+			continue
+			
+def do_child(c,db):
+	#循环接收客户端请求
+	while True:
+		data = c.recv(128).decode()
+		print(c.getpeername(),":",data)
+		if (not data) or data[0] == 'E':
+			c.close()
+			sys.exit(0)
+		if data[0] == 'R':
+			do_register(c,db,data)
+		if data[0] == 'L':
+			do_login(c,db,data)
+		if data[0] == 'Q':
+			do_query(c,db,data)
+		if data[0] == 'H':
+			do_hist(c,db,data)
+
+def do_login(c,db,data):
+	print("登录操作")
+	l = data.split(' ')
+	name = l[1]
+	passwd = l[2]
+	cursor = db.cursor()
+	sql = "select name from user where name = '%s' and passwd = '%s' "%(name,passwd)
+	cursor.execute(sql)
+	r = cursor.fetchone()
+	if r == None:
+		c.send(b'FALL')
+	else:
+		c.send(b'OK')
+		print("%s登录成功"%name)
+	
+def do_register(c,db,data):
+	print("注册操作")
+	l = data.split(' ')
+	name = l[1]
+	passwd = l[2]
+	cursor = db.cursor()
+	sql = "select name from user where name = '%s'"%name
+	cursor.execute(sql)
+	r = cursor.fetchone()
+	if r!= None:
+		c.send(b'EXISTS')
+		return
+	#用户不存在插入用户
+	sql = "insert into user (name,passwd) values ('%s','%s')"%(name,passwd)
+	try:
+		cursor.execute(sql)
+		db.commit()
+		c.send(b'OK')
+	except:
+		db.rollback()
+		c.send(b'FALL')
+	else:
+		print("%s注册成功"%name)
+
+def do_query(c,db,data):
+	print("查询操作")
+	l = data.split(' ')
+	name = l[1]
+	word = l[2]
+	cursor = db.cursor()
+	
+	def insert_history():
+		tm = time.ctime()
+		sql = "insert into hist (name,word,time) values('%s','%s','%s')"%(name,word,tm)
+		try:
+			cursor.execute(sql)
+			db.commit()
+		except:
+			db.rollback()
+	#文本查询
+	try:
+		f = open(DICT_TEXT)
+	except:
+		c.send(b'FALL')
+		return
+	for line in f:
+		tmp = line.split(' ')[0]
+		print(tmp)
+		print(word)
+		#单词不在文本中
+		if tmp > word:
+			c.send(b'FALL')
+			f.close()
+			return 
+		elif tmp == word:
+			c.send(b'OK')
+			time.sleep(0.1)
+			c.send(line.encode())
+			f.close()
+			insert_history()
+			return 
+	c.send(b'FALL')	
+	f.close()
+
+def do_hist(c,db,data):
+	print("历史记录")
+	l = data.split(' ')
+	name = l[1]
+	cursor = db.cursor() 
+	
+	sql = "select * from hist where name = '%s'"%name 
+	cursor.execute(sql)
+	
+	r = cursor.fetchall()
+	if not r:
+		c.send(b'FALL')
+		return
+	else:
+		c.send(b'OK')
+	for i in r:
+		time.sleep(0.1)
+		msg = "%s %s %s"%(i[1],i[2],i[3])
+		c.send(msg.encode())
+	time.sleep(0.1)
+	c.send(b'##')
+	
+if __name__ == '__main__':
+	main()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
