cb6806c7fce50acd1626aeb4d173e49e64fdc63a
zhouwei <zhouweichild@163.com>
2020-01-16 21:23:47 +0800

    增加httpserver第三版项目

---
+++ work/HTTP/httpserver/setting.py
@@ -0,0 +1,11 @@
+#HTTPServer配置文件
+
+HOST = '0.0.0.0'
+PORT = 8000
+ADDR = (HOST,PORT)
+
+#webfarme设置
+
+frame_ip = '127.0.0.1'
+frame_port = 8080
+frame_addr = (frame_ip,frame_port)
---
+++ work/HTTP/webframe/views.py
@@ -0,0 +1,10 @@
+from time import ctime 
+
+def show_time():
+	return ctime()
+
+def say_hello():
+	return "hello python"
+
+def say_bye():
+	return "good bye"	
